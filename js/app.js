34786a4a8e8b221b6bea262c25c3a4a8fc692a78
PolarisChen <polaris_chen@163.com>
2016-01-24 11:16:20 +0800

    Add details

--- js/app.js
+++ js/app.js
@@ -4,7 +4,14 @@ function makeCartScrollNicely() {
   Ps.initialize(cart);
 }
 
+
+
 window.onload = function() {
   console.log("page loaded");
   makeCartScrollNicely();
+
+  var $toggle = document.querySelector(".site__right-sidebar-toggle");
+  $toggle.addEventListener("click",function() {
+    document.body.classList.toggle("js-show-right-sidebar");
+  });
 }
